791dd4d39d51f94afde96526946de83c630f6d23
Oriana <68253167+orianapg@users.noreply.github.com>
2023-02-28 15:20:39 -0300

    Added modules 01 and 02

---
+++ README.md
@@ -0,0 +1,3 @@
+# TypeScript Exercises
+
+From the Microsoft Learn ["Build JavaScript applications using TypeScript"](https://learn.microsoft.com/en-us/training/paths/build-javascript-applications-typescript/) learning path.
---
+++ build/module02_01.js
@@ -0,0 +1,19 @@
+"use strict";
+/*  EXERCISE 1
+    TODO: Modify the code to add types to the variable declarations. */
+let firstName;
+let lastName;
+let fullName;
+let age;
+let ukCitizen;
+firstName = 'Rebecca';
+lastName = 'Smith';
+age = 42;
+ukCitizen = false;
+fullName = firstName + " " + lastName;
+if (ukCitizen) {
+    console.log("My name is " + fullName + ", I'm " + age + ", and I'm a citizen of the United Kingdom.");
+}
+else {
+    console.log("My name is " + fullName + ", I'm " + age + ", and I'm not a citizen of the United Kingdom.");
+}
